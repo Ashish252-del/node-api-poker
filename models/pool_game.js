f28fabfc72b5d609a520f0eead6e237a93d618bd
Amit Garg <amit.garg@digitalcrew.co.in>
2024-08-05 18:07:12 +0530

    latest code

---
+++ models/pool_game.js
@@ -0,0 +1,67 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const attributes = {
+        game_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name : {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        bet_amount: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        percentage: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        table_type: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        target_lines: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        prize: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        win_amount: {
+            type: DataTypes.DECIMAL(15,2),
+            allowNull: false,
+            defaultValue: 0.00
+        },
+        game_status: {
+            type: DataTypes.ENUM,
+            allowNull: false,
+            values: ['0','1','2'],
+            defaultValue: '1',
+        },
+        added_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+    };
+    return sequelize.define('pool_games', attributes,  { timestamps: true });
+}
