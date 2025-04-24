879baa70843baf727d96190f6b3eb4b6e7e4ca04
Ashish <ashishpatel3946@gmail.com>
2025-04-24 15:40:07 +0530

    logs

--- controllers/userController.js
+++ controllers/userController.js
@@ -2442,7 +2442,8 @@ const getMinMaxBuyInForTable = async (userMinMaxBuyInReq) => {
             data["maximum_buyin"] = parseFloat(parseFloat(roomAttributesObj.maximum_buyin+"").toFixed(2));
             data["message"] = "Success"
             data["tableId"] = tableId;
-            data["user_balance"] = parseFloat(parseFloat(balance).toFixed(2));
+            data["user_balance"] = parseFloat(balance.toFixed(2));
+            console.log("data is --->", data);
             return data;
       //  }
 //         // Assuming dbDate is the timestamp retrieved from your database
