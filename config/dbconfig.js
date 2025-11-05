292dae1be86893d8b73a6da80ca0d938d29d0126
Ashish <ashishpatel3946@gmail.com>
2025-11-05 21:49:00 +0530

    changes

---
+++ config/dbconfig.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+//const {initialiseRedis} = require("./helpers/redis");
+dotenv.config();
+const x = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    },
+};
+module.exports = x;
